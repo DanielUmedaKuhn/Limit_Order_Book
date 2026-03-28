52f90433896f8301bf9d43f882afce51098ac247
Daniel Umeda Kuhn <danielumeda1@gmail.com>
2026-03-28 11:48:04 -0300

    refatoração: transição de simulação local para arquitetura de servidor baseada em socket

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,27 +1,18 @@
 package app;
 import engine.MatchingEngine;
-import model.Order;
-import model.Trade;
-import enums.Side;
-import enums.OrderType;
 import database.DatabaseConfig;
-import java.util.*;
+import network.TradingServer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Simulando.\n");
-        DatabaseConfig.initializeDatabase();
+        System.out.println("Inicializando Trading Server...\n");
+        DatabaseConfig.initializeDatabase();  //Infraestrutura de dados (SQLite)
         MatchingEngine engine = new MatchingEngine();
         engine.rebuildBookFromDatabase();  //Carrega o que estava no banco para a RAM
 
-        long id1 = System.currentTimeMillis();
-        //engine.submitOrder(new Order(id1, 100, 10, 10, Side.SELL,OrderType.LIMIT));
-
-        long id2 = System.currentTimeMillis();
-        //engine.submitOrder(new Order(id2, 110, 5, 5, Side.SELL, OrderType.LIMIT));
-
-        long id3 = System.currentTimeMillis();
-        engine.submitOrder(new Order(id3, 100, 10, 10, Side.BUY, OrderType.LIMIT));
-        System.out.print("Orders enviadas. Verifique o banco de dados e feche o programa.");
+        int porta = 8080;
+        TradingServer server = new TradingServer(porta, engine);
+        System.out.println("[INFO] Servidor aguardando conexões na porta " + porta + "...");
+        server.start();  //Programa ficará rodando até ser parado manualmente
     }
 }
